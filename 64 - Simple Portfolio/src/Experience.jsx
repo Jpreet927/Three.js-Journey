9457fe5476e72eaf1837a0c3cbcbbeb6c4d1d30e
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-07-11 19:11:29 -0400

    :computer: simple portfolio

---
+++ 64 - Simple Portfolio/src/Experience.jsx
@@ -0,0 +1,80 @@
+import {
+    PresentationControls,
+    useGLTF,
+    Environment,
+    Float,
+    ContactShadows,
+    Html,
+    Text,
+} from "@react-three/drei";
+import {
+    Bloom,
+    EffectComposer,
+    ToneMapping,
+    Vignette,
+} from "@react-three/postprocessing";
+import { ToneMappingMode, BlendFunction, GlitchMode } from "postprocessing";
+
+export default function Experience() {
+    const laptop = useGLTF(
+        "https://threejs-journey.com/resources/models/macbook_model.gltf"
+    );
+
+    return (
+        <>
+            <Environment preset="city" />
+            <color args={["#03070f"]} attach="background" />
+
+            <PresentationControls
+                global
+                rotation={[0.13, 0.1, 0]}
+                polar={[-0.4, 0.2]}
+                azimuth={[-1, 0.75]}
+                config={{ mass: 2, tension: 400 }}
+                snap={{ mass: 4, tension: 400 }}
+            >
+                <Float rotationIntensity={0.4}>
+                    <rectAreaLight
+                        width={2.5}
+                        height={1.65}
+                        intensity={65}
+                        color={"#003e9c"}
+                        rotation={[-0.1, Math.PI, 0]}
+                        position={[0, 0.55, -1.15]}
+                    />
+                    <primitive object={laptop.scene} position-y={-1.2}>
+                        <Html
+                            transform
+                            wrapperClass="htmlScreen"
+                            distanceFactor={1.17}
+                            position={[0, 1.56, -1.4]}
+                            rotation-x={-0.256}
+                        >
+                            <iframe
+                                src="https://jaipreet.ca"
+                                frameborder="0"
+                            ></iframe>
+                        </Html>
+                    </primitive>
+                    <Text
+                        fontSize={1.4}
+                        fontWeight={700}
+                        position={[2, 0.75, 0.75]}
+                        rotation-y={-1.25}
+                        maxWidth={2}
+                        textAlign="center"
+                        color={"#ffffff"}
+                    >
+                        JP
+                    </Text>
+                </Float>
+            </PresentationControls>
+            <ContactShadows
+                position-y={-1.4}
+                opacity={0.4}
+                scale={5}
+                blur={2.4}
+            />
+        </>
+    );
+}
